4d8851fd2ac733f86fabf363c3b166869a10035b
Jovanni Elias <jovanni.elias@g.austincc.edu>
2020-02-06 19:45:28 -0600

    testing

---
+++ main.js
@@ -0,0 +1,10 @@
+/*********************
+Name: Jovanni Elias
+Coding 03
+Purpose: The purpose of the assignment is to create a working contact form with JavaScript.
+         It will accept a  first name, last name, email and subject section 
+         which is no longer than 64 characters. The message section will be limited to 1000 characters.
+
+Notes: This is the beginning of the assignment.
+**********************/
+"use strict";
